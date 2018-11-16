c1ba618f77337fef7c48e1e215888a9758d190c1
Nathan Brennan <nbre004@ucr.edu>
2018-11-15 23:17:22 -0800

    started copy constructor

--- command.h
+++ command.h
@@ -12,11 +12,23 @@ class Command: public Base{
     std::vector<Base*> argumentList;
 
   public:
-    Command():Base(){};
-    Command( char* repr ):Base(repr){};
+    /* Constructors */
+    Command() : Base() {};
+    Command(char* repr) : Base(repr){};
+    /* Copy Constructor */
+    /*
+    Command(const Command RHS) {
+        for (auto basePtr : RHS.argumentList) {
+            Base* temp = new Argument()
+                // YOU'RE NOT DONE
+            this->argumentList.push_back(temp);
+        }
+    }
+    */
     void run();
-    void add( Base* argument ){ argumentList.push_back(argument); };
+    void add(Base* argument) { argumentList.push_back(argument); };
 };
+
 
 void Command::run(){
   char* argc [argumentList.size() + 2];

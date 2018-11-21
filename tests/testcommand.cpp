d31ca5f34125cd12da20025387332de603fb3b9a
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2018-11-21 15:56:00 -0800

    removed debug prints and created valid tests

--- tests/testcommand.cpp
+++ tests/testcommand.cpp
@@ -1,62 +1,119 @@
 #include <iostream>
 
 #include "../src/command.h"
+#include "../src/argument.h"
+#include <sys/wait.h>
 
+int test_valid_command();
+int test_invalid_command();
+int test_empty_command();
+int test_exit_command();
+int test_valid_command_with_argument();
+int test_valid_command_with_empty_argument();
+int test_invalid_command_with_argument();
 
-void test_valid_command(){
-
-    Base* head = new Command("ls");
-        
-    pid_t pid = fork();
-
-    if(pid = 0){
-      head->run();
-    }
-    else{
-      waitpid(pid,nullptr,0);
-      delete head;
-    }
-
+int main(){
+  std::cout<<"VALID COMMAND: "<<test_valid_command()<<std::endl;
+  std::cout<<"INVALID COMMAND: "<<test_invalid_command()<<std::endl; 
+  return 0;
 }
 
-void test_invalid_command(){
-  Base* head = new Command("qs");
-        
-  pid_t pid = fork();
 
-  if(pid = 0){
-    head->run();
-  }
-  else{
-    waitpid(pid,nullptr,0);
-    delete head;
-  }
-}
 
-void test_valid_command_with_argument(){
+int test_valid_command(){
+
   Base* head = new Command("ls");
-  head->add(new Argument("-a")
+  int exitVal = 0;      
   pid_t pid = fork();
 
-  if(pid = 0){
+  if(pid == 0){
     head->run();
+    exit(errno);
   }
   else{
-    waitpid(pid,nullptr,0);
+    waitpid(pid,&exitVal,0);
     delete head;
   }
+  return WEXITSTATUS(exitVal);
 }
 
-void test_invalid_command_with_argument(){
+int test_invalid_command(){
   Base* head = new Command("qs");
-  head->add(new Argument("-a")
+  int exitVal = 0;      
   pid_t pid = fork();
 
-  if(pid = 0){
+  if(pid == 0){
+    head->run();
+    exit(errno);
+  }
+  else{
+    waitpid(pid,&exitVal,0);
+    delete head;
+  }
+  return WEXITSTATUS(exitVal);
+}
+
+int test_empty_command(){
+
+  Base* head = new Command("");
+  int exitVal = 0;
+  pid_t pid = fork();
+
+  if(pid == 0){
     head->run();
   }
   else{
-    waitpid(pid,nullptr,0);
+    waitpid(pid,&exitVal,0);
+    delete head;
+    return WEXITSTATUS(exitVal);
+  }
+  return WEXITSTATUS(exitVal);
+}
+
+int test_exit_command(){
+
+  Base* head = new Command("exit");
+  int exitVal = 0;      
+  pid_t pid = fork();
+
+  if(pid == 0){
+    head->run();
+  }
+  else{
+    waitpid(pid,&exitVal,0);
     delete head;
   }
+  return WEXITSTATUS(exitVal);
+}
+
+int test_valid_command_with_argument(){
+  Base* head = new Command("ls");
+  head->add(new Argument("-a"));
+  int exitVal = 0; 
+  pid_t pid = fork();
+
+  if(pid == 0){
+    head->run();
+  }
+  else{
+    waitpid(pid,&exitVal,0);
+    delete head;
+  }
+  return WEXITSTATUS(exitVal);
+}
+
+int test_invalid_command_with_argument(){
+  Base* head = new Command("qs");
+  head->add(new Argument("-a"));
+  int exitVal = 0;
+  pid_t pid = fork();
+
+  if(pid == 0){
+    head->run();
+  }
+  else{
+    waitpid(pid,&exitVal,0);
+    delete head;
+  }
+  return WEXITSTATUS(exitVal);
 }
